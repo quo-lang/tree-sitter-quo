a819d1dac2f8eabbbd95ac1de1e93650413cb3b0
mrvladus <mrvladus@yandex.ru>
2026-08-20 14:25:02 +0300

    Upd

--- grammar.js
+++ grammar.js
@@ -32,8 +32,8 @@ export default grammar({
       ),
 
     return_statement: ($) => seq("return", optional($.expression)),
-    break_statement: (_) => "break",
-    continue_statement: (_) => "continue",
+    break_statement: (_) => token("break"),
+    continue_statement: (_) => token("continue"),
 
     expression: ($) => choice($.identifier, $.string, $.number),
 
