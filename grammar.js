909614206e9c1957d519653adeed0cb1b457cd82
mrvladus <mrvladus@yandex.ru>
2026-08-20 14:50:45 +0300

    Upd

--- grammar.js
+++ grammar.js
@@ -5,7 +5,21 @@
  */
 
 /// <reference types="tree-sitter-cli/dsl" />
-// @ts-check
+
+const PREC = {
+  or: 1,
+  and: 2,
+  equality: 3,
+  comparison: 4,
+  term: 5,
+  factor: 6,
+  unary: 7,
+  call: 8,
+};
+
+function commaSeparate(rule) {
+  return optional(seq(rule, repeat(seq(",", rule)), optional(",")));
+}
 
 export default grammar({
   name: "quo",
@@ -19,9 +33,11 @@ export default grammar({
     _statement: ($) =>
       choice(
         $.var_statement,
+        $.block_statement,
         $.return_statement,
         $.break_statement,
         $.continue_statement,
+        $.expression_statement,
       ),
 
     var_statement: ($) =>
@@ -30,22 +46,35 @@ export default grammar({
         field("name", $.identifier),
         optional(seq("=", field("value", $.expression))),
       ),
-
-    return_statement: ($) => seq("return", optional($.expression)),
+    block_statement: ($) => prec(1, seq("{", repeat($._statement), "}")),
+    return_statement: ($) => prec.right(seq("return", optional($.expression))),
     break_statement: (_) => seq("break"),
     continue_statement: (_) => seq("continue"),
+    expression_statement: ($) => prec(1, $.expression),
 
+    // Expressions
     expression: ($) => choice($.literal),
 
     // Literals
-    literal: ($) => choice($.string, $.number, $.boolean, $.nil),
+    literal: ($) =>
+      choice($.string, $.number, $.boolean, $.nil, $.array, $.dictionary),
 
     identifier: (_) => /[A-Za-z_][A-Za-z0-9_]*/,
     string: (_) => /"([^"\\]|\\.)*"/,
     number: (_) => /\d[_\d]*(\.\d[_\d]*)?/,
     boolean: (_) => token(choice("true", "false")),
     nil: (_) => "nil",
+    array: ($) => seq("[", commaSeparate($.expression), "]"),
+    dictionary: ($) =>
+      seq(
+        "{",
+        commaSeparate(
+          seq(field("key", $.string), ":", field("value", $.expression)),
+        ),
+        "}",
+      ),
 
+    // Comments
     line_comment: (_) => token(seq("#", /[^\r\n]*/)),
   },
 });
